b3789ee85d6195424d65001c12b2943433a4544f
Arman Abbasi <arman.abbasi.nt@gmail.com>
2022-11-27 00:25:42 +0330

    changes on login tests

--- Pages/login.py
+++ Pages/login.py
@@ -1,12 +1,13 @@
 class Login:
-    def __int__(self,driver):
-        self.driver = driver
-    self.name_textbox_id = "form-field-name"
-    self.phonenumber_textbox_id = "form-field-email"
-    self.message_textbox_id = "form-field-message"
-    self.send_button_class = "elementor-button elementor-size-sm"
 
-#we set attribute in this class and we can use all functional and other thing than use from this class, we can use from this attribute
+    def __init__(self,driver):
+        self.driver = driver
+        self.name_textbox_id = "form-field-name"
+        self.phonenumber_textbox_id = "form-field-email"
+        self.message_textbox_id = "form-field-message"
+        self.send_button_class = "elementor-button elementor-size-sm"
+
+#we set attribute in this class and we can use all functional and other thing that using from this class, we can use from this attribute
 
     def enter_name (self, name):
         self.driver.find_element ('id', self.name_textbox_id).send_keys(name)
