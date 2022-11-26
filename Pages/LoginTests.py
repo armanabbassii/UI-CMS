f94b114b03f53f3177e59fcfdcc6bf283ece915b
Arman Abbasi <arman.abbasi.nt@gmail.com>
2022-11-26 23:29:50 +0330

    changes on login tests

--- Pages/LoginTests.py
+++ Pages/LoginTests.py
@@ -1,4 +1,11 @@
-from login import Login
-#when we must import a class in a file
 
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+from login import Login
+
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+
+#when we must import a class in a file
+# when we want to use a object the have all value of class.
 login_object = Login()
