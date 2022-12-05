1040ce1932de747a5f11807c992b8cc6d679b89f
Arman Abbasi <arman.abbasi.nt@gmail.com>
2022-12-06 01:55:30 +0330

    locators

--- Pages/login.py
+++ Pages/login.py
@@ -2,14 +2,7 @@ class Login:
 
     def __init__(self,driver):
         self.driver = driver
-        self.name_textbox_id = "form-field-name"
-        self.phonenumber_textbox_id = "form-field-email"
-        self.message_textbox_id = "form-field-message"
-        #self.send_button_class = driver.find_element(By.xpath, "//*[@class = 'elementor-button-text '][1]")
-       # self.send_button_class = "//span[@className= 'elementor-button-text'][1]"
-        #self.send_button_class = "(//span[@class= 'elementor-button-text'])[1]"
-        #self.send_button_class = "//span[text()= 'ارسال'][1]"
-        self.send_button_class = "elementor-button-text"
+
     def enter_name (self, name):
         self.driver.find_element ('id', self.name_textbox_id).send_keys(name)
 
