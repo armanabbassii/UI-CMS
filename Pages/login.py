bedc8ce5d3e2d5eab0841cee1ace6088b317c05a
Arman Abbasi <arman.abbasi.nt@gmail.com>
2022-12-06 20:24:01 +0330

    change all code for testing git

--- Pages/login.py
+++ Pages/login.py
@@ -2,6 +2,12 @@ class Login:
 
     def __init__(self,driver):
         self.driver = driver
+        self.name_textbox_id = "form-field-name"
+        self.phonenumber_textbox_id = "form-field-email"
+        self.message_textbox_id = "form-field-message"
+        self.send_button_class = "elementor-button elementor-size-sm"
+
+#we set attribute in this class and we can use all functional and other thing that using from this class, we can use from this attribute
 
     def enter_name (self, name):
         self.driver.find_element ('id', self.name_textbox_id).send_keys(name)
@@ -13,7 +19,4 @@ class Login:
         self.driver.find_element ('id', self.message_textbox_id).send_keys(message)
 
     def click_on_send_button (self):
-        self.driver.find_element('xpath',"//*[@class ='elementor-button-text'][1]").click()
-
-
-    # we set attribute in this class and we can use all functional and other thing that using from this class, we can use from this attribute
+        self.driver.find_element ('class',self.send_button_class).click()
