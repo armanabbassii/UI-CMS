7e9297626161d811089def5694f1c58a92d09de6
armanabbassii <arman.abbasi.nt@gmail.com>
2022-12-11 20:51:55 +0330

    set config

--- Locators.py
+++ Locators.py
@@ -1,11 +1,10 @@
-name_textbox_id        = "form-field-name"
+name_textbox_id = "form-field-name"
 phonenumber_textbox_id = "form-field-email"
-message_textbox_id     = "form-field-message"
-send_button_class      = "(//span[@class= 'elementor-button-text'])[1]"
-
+message_textbox_id = "form-field-message"
+send_button_class = "(//span[@class= 'elementor-button-text'])[1]"
 
 # variable name        = variable value
-        #self.send_button_class = driver.find_element(By.xpath, "//*[@class = 'elementor-button-text '][1]")
-       # self.send_button_class = "//span[@className= 'elementor-button-text'][1]"
-        #self.send_button_class = "(//span[@class= 'elementor-button-text'])[1]"
-        #self.send_button_class = "//span[text()= 'ارسال'][1]"
+# self.send_button_class = driver.find_element(By.xpath, "//*[@class = 'elementor-button-text '][1]")
+# self.send_button_class = "//span[@className= 'elementor-button-text'][1]"
+# self.send_button_class = "(//span[@class= 'elementor-button-text'])[1]"
+# self.send_button_class = "//span[text()= 'ارسال'][1]"
